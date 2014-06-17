2bd7a08b22144c10763abe736070be1301c4f4ec
servanter <fengshang@126.com>
2014-06-17 22:35:03 +0800

    增加music属性 Signed-off-by: servanter <fengshang@126.com>

--- src/main/java/com/zhy/listen/bean/Music.java
+++ src/main/java/com/zhy/listen/bean/Music.java
@@ -1,5 +1,59 @@
 package com.zhy.listen.bean;
 
-public class Music {
+public class Music extends Paging implements java.io.Serializable{
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -2909860277973637781L;
+
+    private Long id;
+    
+    private String title;
+    
+    private String author;
+    
+    private String url;
+    
+    private String lrc;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLrc() {
+        return lrc;
+    }
+
+    public void setLrc(String lrc) {
+        this.lrc = lrc;
+    }
 }
